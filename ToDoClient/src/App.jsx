54ca6193997889c1633f65d88e391d430e3a8e69
Creed <laurvitalik89@gmail.com>
2026-01-18 18:45:33 +0200

    feat: initialize To Do Client react project with tailwind css v4 and postcss

---
+++ ToDoClient/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-extrabold tracking-tighter text-red-950'>To Do Client</h1>
+    </div>
+  )
+}
+
+export default App
